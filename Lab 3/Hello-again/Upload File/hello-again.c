8d62b21915589ddce194be7f72246d56d6e3ba13
Harish-na <Harishns2001@gmail.com>
2020-11-02 17:04:30 +0000

    New Lab Tasks

---
+++ Lab 3/Hello-again/Upload File/hello-again.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h> /* a header file with a function we might need */
+
+int main(int argc, char * argv[1]) /* Takes command line argument */
+{
+   printf("Hello %s\n",argv[1]);
+   exit(0);
+}
